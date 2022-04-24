7f9b00942c08ec703b61afff9a71738fc0b14032
jkurdziel <jakub.kurdziel@talkie.ai>
2022-04-24 22:28:18 +0200

    add more example code

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ const result = _.chain(users)
   .filter((user) => user.age > 25)
   .map((user) => user.name)
   .value();
-
 console.log(result);
 
 function sayHello(name: string): string {
@@ -26,4 +25,37 @@ const helloYou = (name: string) => {
   name = 'you' || name;
   console.log('hello' + name + '!');
 };
+
 const a = 'a';
+
+const data = [
+  { product: 'apple', price: 100 },
+  { product: 'banana', price: 200 },
+  { product: 'orange', price: 300 },
+  { product: 'apple', price: 400 },
+];
+
+// group by product
+
+const result2 = _.chain(data)
+  .groupBy('product')
+  .map((group) => {
+    return {
+      product: group[0].product,
+      price: _.sumBy(group, 'price'),
+    };
+  })
+  .value();
+
+console.log(result2);
+
+class Person {
+  private readonly name: string;
+  private readonly age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const me = new Person('ryan', 35);
