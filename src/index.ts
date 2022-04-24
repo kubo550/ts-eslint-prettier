53bc4c11a3897112245c01e8e295ed76741538b8
jkurdziel <jakub.kurdziel@talkie.ai>
2022-04-24 22:19:31 +0200

    add code example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
-const a = 'dsad';
+import _ from 'lodash';
 
-const bbb = '';
+const users = [
+  { name: 'ryan', age: 35 },
+  { name: 'lucy', age: 30 },
+  { name: 'bob', age: 25 },
+  { name: 'alice', age: 20 },
+];
 
-let c: any;
-console.log(43);
+const result = _.chain(users)
+  .filter((user) => user.age > 25)
+  .map((user) => user.name)
+  .value();
+
+console.log(result);
+
+function sayHello(name: string): string {
+  return `Hello, ${name}`;
+}
+
+users.forEach((user) => {
+  console.log(sayHello(user.name));
+});
+
+const helloYou = (name: string) => {
+  name = 'you' || name;
+  console.log('hello' + name + '!');
+};
+const a = 'a';
